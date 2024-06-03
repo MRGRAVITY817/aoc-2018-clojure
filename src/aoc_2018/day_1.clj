d017f3279ac726f0778d5cf0eaf6238871da0479
Hoon Wee <mrgravity817@gmail.com>
2024-06-03 18:58:25 +0900

    day 1 part 2 solved + refactor part 1 code

--- src/aoc_2018/day_1.clj
+++ src/aoc_2018/day_1.clj
@@ -13,13 +13,37 @@
   "Calculate the resulting frequency from a list of frequency changes."
   [input]
   (->> input
-       parse-frequency-changes
        (reduce + 0)))
 
-(defn day-1 [input]
+(defn day-1-part-1
+  "Calculate the resulting frequency from a file."
+  [input]
   (->> input
        slurp
+       parse-frequency-changes
        resulting-frequency))
 
+(defn twice-reached-frequency
+  "Find the first frequency that is reached twice."
+  [input]
+  (loop [frequencies       #{0}
+         current-frequency 0
+         frequency-changes (cycle input)]
+    (let [new-frequency (+ current-frequency (first frequency-changes))]
+      (if (contains? frequencies new-frequency)
+        new-frequency
+        (recur (conj frequencies new-frequency)
+               new-frequency
+               (rest frequency-changes))))))
+
+(defn day-1-part-2
+  "Find the first frequency that is reached twice from a file."
+  [input]
+  (->> input
+       slurp
+       parse-frequency-changes
+       twice-reached-frequency))
+
 (comment
-  (day-1 "resources/day_1_input.txt")) ; 599
+  (day-1-part-1 "resources/day_1_input.txt") ; 599
+  (day-1-part-2 "resources/day_1_input.txt")) ; 81204
--- test/aoc_2018/day_1_test.clj
+++ test/aoc_2018/day_1_test.clj
@@ -2,14 +2,6 @@
   (:require [clojure.test :refer [deftest is testing]]
             [aoc-2018.day-1 :as sut]))
 
-(deftest test-resulting-frequency
-  (testing "+1 +1 +1"
-    (is (= 3 (sut/resulting-frequency "+1\r\n+1\r\n+1"))))
-  (testing "-1 +1 -1"
-    (is (= -1 (sut/resulting-frequency "-1\r\n+1\r\n-1"))))
-  (testing "-2 -3 +4 -15 -15 +18 -7 +11 -16 -14 +2 -6 +16 +6"
-    (is (= -21 (sut/resulting-frequency "-2\r\n-3\r\n+4\r\n-15\r\n-15\r\n+18\r\n-7\r\n+11\r\n-16\r\n-14\r\n+2\r\n-6\r\n+16\r\n+6")))))
-
 (deftest test-parse-frequency-changes
   (testing "+1 +1 +1"
     (is (= [1 1 1] (sut/parse-frequency-changes "+1\r\n+1\r\n+1"))))
@@ -17,3 +9,19 @@
     (is (= [-1 1 -1] (sut/parse-frequency-changes "-1\r\n+1\r\n-1"))))
   (testing "-2 -3 +4 -15 -15 +18 -7 +11 -16 -14 +2 -6 +16 +6"
     (is (= [-2 -3 4 -15 -15 18 -7 11 -16 -14 2 -6 16 6] (sut/parse-frequency-changes "-2\r\n-3\r\n+4\r\n-15\r\n-15\r\n+18\r\n-7\r\n+11\r\n-16\r\n-14\r\n+2\r\n-6\r\n+16\r\n+6")))))
+
+(deftest test-resulting-frequency
+  (testing "+1 +1 +1"
+    (is (= 3 (sut/resulting-frequency [1 1 1]))))
+  (testing "-1 +1 -1"
+    (is (= -1 (sut/resulting-frequency [-1 1 -1]))))
+  (testing "-2 -3 +4 -15 -15 +18 -7 +11 -16 -14 +2 -6 +16 +6"
+    (is (= -21 (sut/resulting-frequency [-2 -3 4 -15 -15 18 -7 11 -16 -14 2 -6 16 6])))))
+
+(deftest test-twice-reached-frequency
+  (testing "+1 -1 +1"
+    (is (= 0 (sut/twice-reached-frequency [1 -1]))))
+  (testing "+3 +3 +4 -2 -4"
+    (is (= 10 (sut/twice-reached-frequency [3 3 4 -2 -4]))))
+  (testing "-6 +3 +8 +5 -6"
+    (is (= 5 (sut/twice-reached-frequency [-6 3 8 5 -6])))))
