93be7b853d643ac8e5404301f20d201a45eb3bcc
Hoon Wee <mrgravity817@gmail.com>
2024-06-06 10:45:50 +0900

    feat: get overlapping area

--- src/aoc_2018/day_3.clj
+++ src/aoc_2018/day_3.clj
@@ -1,4 +1,5 @@
-(ns aoc-2018.day-3)
+(ns aoc-2018.day-3
+  (:require [clojure.set :refer [intersection]]))
 
 (def claim-regex #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
@@ -18,6 +19,17 @@
                j (range h)]
            {:id id :coords [(+ x i) (+ y j)]}))))
 
+(defn overlapping-area
+  "Find the overlapping area between two sets of coordinates."
+  [area1 area2]
+  (let [coords1 (->> area1 (map :coords) set)
+        coords2 (->> area2 (map :coords) set)
+        overlapping (intersection coords1 coords2)]
+    overlapping))
+
 (comment
-  (parse-claim "#1 @ 1,3: 4x4") ; => [1 1 3 4 4]
+  (parse-claim "#1 @ 1,3: 2x2") ; #{{:id 1, :coords [2 3]} {:id 1, :coords [2 4]} {:id 1, :coords [1 4]} {:id 1, :coords [1 3]}}
+  (map :coords #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}})  ; ([1 2] [1 1] [1 4] [1 3])
+  (overlapping-area #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
+                    #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}})  ; #{[1 1] [1 2]}
   )
--- test/aoc_2018/day_3_test.clj
+++ test/aoc_2018/day_3_test.clj
@@ -9,5 +9,12 @@
            {:id 1, :coords [4 3]} {:id 1, :coords [4 4]} {:id 1, :coords [4 5]} {:id 1, :coords [4 6]}}
          (sut/parse-claim "#1 @ 1,3: 4x4"))))
 
+(deftest test-overlapping-area
+  (is (= #{[1 1] [1 2]}
+         (sut/overlapping-area #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]}
+                                 {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
+                               #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]}
+                                 {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}))))
+
 
 
