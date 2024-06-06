4c08a4586e571321c961b3f385a6ffbf0e52afb1
Hoon Wee <mrgravity817@gmail.com>
2024-06-06 20:13:43 +0900

    feat: parse and concat claim areas

--- src/aoc_2018/day_3.clj
+++ src/aoc_2018/day_3.clj
@@ -1,5 +1,6 @@
 (ns aoc-2018.day-3
-  (:require [clojure.set :refer [intersection union]]))
+  (:require [clojure.set :refer [intersection union]]
+            [clojure.string :as str]))
 
 (def claim-regex #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
@@ -15,39 +16,58 @@
                           (re-matches claim-regex)
                           rest
                           (mapv #(Integer/parseInt %)))]
-    (set (for [i (range w)
-               j (range h)]
-           {:id id :coords [(+ x i) (+ y j)]}))))
+    (for [i (range w)
+          j (range h)]
+      {:id id :coords [(+ x i) (+ y j)]})))
 
-(defn overlapping-area
-  "Find the overlapping area between two sets of coordinates."
-  [area1 area2]
-  (let [coords1 (->> area1 (map :coords) set)
-        coords2 (->> area2 (map :coords) set)
-        overlapping (intersection coords1 coords2)]
-    overlapping))
+(defn parse-and-concat
+  "Parse and concatenate a list of claims."
+  [inputs]
+  (mapcat parse-claim inputs))
 
-(defn overlap-within
-  [coord-sets]
-  (loop [[current & others] coord-sets
-         overlap            #{}]
-    (if (empty? others)
-      overlap
-      (recur others
-             (->> others
-                  (map #(overlapping-area current %))
-                  (reduce union overlap))))))
+#_(defn overlapping-area
+    "Find the overlapping area between two sets of coordinates."
+    [area1 area2]
+    (let [coords1 (->> area1 (map :coords) set)
+          coords2 (->> area2 (map :coords) set)
+          overlapping (intersection coords1 coords2)]
+      overlapping))
+
+#_(defn overlap-within
+    [coord-sets]
+    (loop [[current & others] coord-sets
+           overlap            #{}]
+      (if (empty? others)
+        overlap
+        (recur others
+               (->> others
+                    (map #(overlapping-area current %))
+                    (reduce union overlap))))))
+
+#_(defn day-3-part-1
+    "Calculate the overlapping area of a list of claims."
+    [filename]
+    (->> filename
+         slurp
+         (str/split-lines)
+         (map parse-claim)
+         overlap-within
+         count))
 
 (comment
   (parse-claim "#1 @ 1,3: 2x2") ; #{{:id 1, :coords [2 3]} {:id 1, :coords [2 4]} {:id 1, :coords [1 4]} {:id 1, :coords [1 3]}}
   (map :coords #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}})  ; ([1 2] [1 1] [1 4] [1 3])
-  (overlapping-area #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
-                    #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}})  ; #{[1 1] [1 2]}
-  (overlapping-area #{{:id 3, :coords [1 1]} {:id 3, :coords [1 2]} {:id 3, :coords [1 3]} {:id 3, :coords [1 4]}}
-                    #{{:id 4, :coords [1 1]} {:id 4, :coords [1 2]} {:id 4, :coords [1 3]} {:id 4, :coords [1 4]}}) ; #{[1 1] [1 4] [1 3] [1 2]}
+  #_(overlapping-area #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
+                      #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}})  ; #{[1 1] [1 2]}
+  #_(overlapping-area #{{:id 3, :coords [1 1]} {:id 3, :coords [1 2]} {:id 3, :coords [1 3]} {:id 3, :coords [1 4]}}
+                      #{{:id 4, :coords [1 1]} {:id 4, :coords [1 2]} {:id 4, :coords [1 3]} {:id 4, :coords [1 4]}}) ; #{[1 1] [1 4] [1 3] [1 2]}
   (into #{} #{[1 2] [2 3]}) ; #{1 2 3}
-  (overlap-within [#{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
-                   #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}
-                   #{{:id 3, :coords [1 1]} {:id 3, :coords [1 2]} {:id 3, :coords [1 3]} {:id 3, :coords [1 4]}}
-                   #{{:id 4, :coords [1 1]} {:id 4, :coords [1 2]} {:id 4, :coords [1 3]} {:id 4, :coords [1 4]}}]) ; #{[1 1] [1 4] [1 3] [1 2]}
+  #_(overlap-within [#{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
+                     #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}
+                     #{{:id 3, :coords [1 1]} {:id 3, :coords [1 2]} {:id 3, :coords [1 3]} {:id 3, :coords [1 4]}}
+                     #{{:id 4, :coords [1 1]} {:id 4, :coords [1 2]} {:id 4, :coords [1 3]} {:id 4, :coords [1 4]}}]) ; #{[1 1] [1 4] [1 3] [1 2]}
+  #_(day-3-part-1 "resources/day_3_input.txt")
+  ;
   )
+
+
--- test/aoc_2018/day_3_test.clj
+++ test/aoc_2018/day_3_test.clj
@@ -3,25 +3,31 @@
             [aoc-2018.day-3 :as sut]))
 
 (deftest test-parse-claim
-  (is (= #{{:id 1, :coords [1 3]} {:id 1, :coords [1 4]} {:id 1, :coords [1 5]} {:id 1, :coords [1 6]}
-           {:id 1, :coords [2 3]} {:id 1, :coords [2 4]} {:id 1, :coords [2 5]} {:id 1, :coords [2 6]}
-           {:id 1, :coords [3 3]} {:id 1, :coords [3 4]} {:id 1, :coords [3 5]} {:id 1, :coords [3 6]}
-           {:id 1, :coords [4 3]} {:id 1, :coords [4 4]} {:id 1, :coords [4 5]} {:id 1, :coords [4 6]}}
+  (is (= [{:id 1, :coords [1 3]} {:id 1, :coords [1 4]} {:id 1, :coords [1 5]} {:id 1, :coords [1 6]}
+          {:id 1, :coords [2 3]} {:id 1, :coords [2 4]} {:id 1, :coords [2 5]} {:id 1, :coords [2 6]}
+          {:id 1, :coords [3 3]} {:id 1, :coords [3 4]} {:id 1, :coords [3 5]} {:id 1, :coords [3 6]}
+          {:id 1, :coords [4 3]} {:id 1, :coords [4 4]} {:id 1, :coords [4 5]} {:id 1, :coords [4 6]}]
          (sut/parse-claim "#1 @ 1,3: 4x4"))))
 
-(deftest test-overlapping-area
-  (is (= #{[1 1] [1 2]}
-         (sut/overlapping-area #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]}
-                                 {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
-                               #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]}
-                                 {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}))))
+(deftest test-parse-and-concat
+  (is (= [{:id 1, :coords [1 3]} {:id 1, :coords [1 4]} {:id 1, :coords [2 3]} {:id 1, :coords [2 4]}
+          {:id 2, :coords [1 3]} {:id 2, :coords [1 4]} {:id 2, :coords [2 3]} {:id 2, :coords [2 4]}]
+         (sut/parse-and-concat ["#1 @ 1,3: 2x2" "#2 @ 1,3: 2x2"]))))
 
-(deftest test-overlap-within
-  (is (= #{[1 1] [1 4] [1 3] [1 2]}
-         (sut/overlap-within [#{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
-                              #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}
-                              #{{:id 3, :coords [1 1]} {:id 3, :coords [1 2]} {:id 3, :coords [1 3]} {:id 3, :coords [1 4]}}
-                              #{{:id 4, :coords [1 1]} {:id 4, :coords [1 2]} {:id 4, :coords [1 3]} {:id 4, :coords [1 4]}}]))))
+#_(deftest test-overlapping-area
+    (is (= #{[1 1] [1 2]}
+           (sut/overlapping-area #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]}
+                                   {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
+                                 #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]}
+                                   {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}))))
+
+#_(deftest test-overlap-within
+    (is (= #{[1 1] [1 4] [1 3] [1 2]}
+           (sut/overlap-within [#{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}}
+                                #{{:id 2, :coords [1 1]} {:id 2, :coords [1 2]} {:id 2, :coords [2 1]} {:id 2, :coords [2 2]}}
+                                #{{:id 3, :coords [1 1]} {:id 3, :coords [1 2]} {:id 3, :coords [1 3]} {:id 3, :coords [1 4]}}
+                                #{{:id 4, :coords [1 1]} {:id 4, :coords [1 2]} {:id 4, :coords [1 3]} {:id 4, :coords [1 4]}}]))))
+
 
 
 
