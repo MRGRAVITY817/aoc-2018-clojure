63ac7f6f9f7129a235f4ab609f50068f82665beb
Hoon Wee <mrgravity817@gmail.com>
2024-06-07 22:03:56 +0900

    feat: use malli to check fn schema

--- src/aoc_2018/day_3.clj
+++ src/aoc_2018/day_3.clj
@@ -1,9 +1,23 @@
 (ns aoc-2018.day-3
   (:require [clojure.string :as str]
-            [clojure.set :refer [subset?]]))
+            [clojure.set :refer [subset?]]
+            [malli.core :as m]
+            [malli.dev :as mdev]))
+
+(mdev/start!)
+
+(def Coordinate
+  [:vector :int])
+
+(def Claim
+  [:sequential
+   [:map
+    [:id :int]
+    [:coords Coordinate]]])
 
 (def claim-regex #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
+(m/=> parse-claim [:=> [:cat :string] [:maybe Claim]])
 (defn parse-claim
   "Parse a claim from a string and return a set of coordinates.
    Example:
@@ -16,10 +30,12 @@
                           (re-matches claim-regex)
                           rest
                           (mapv #(Integer/parseInt %)))]
-    (for [i (range w)
-          j (range h)]
-      {:id id :coords [(+ x i) (+ y j)]})))
+    (when (and id x y w h)
+      (for [i (range w)
+            j (range h)]
+        {:id id :coords [(+ x i) (+ y j)]}))))
 
+(m/=> parse-and-concat [:=> [:cat :string] [:sequential Coordinate]])
 (defn parse-and-concat
   "Parse and concatenate a list of claims."
   [input]
@@ -27,6 +43,7 @@
        str/split-lines
        (mapcat parse-claim)))
 
+(m/=> count-overlapping-areas [:=> [:cat [:sequential Coordinate]] :int])
 (defn count-overlapping-areas
   "Count areas that has been overlapped by more than two claims."
   [areas]
@@ -36,12 +53,14 @@
        (filter #(>= (val %) 2))
        count))
 
+(m/=> day-3-part-1 [:=> [:cat :string] :int])
 (defn day-3-part-1
   "Calculate the overlapping area of a list of claims."
   [filename]
   (-> filename slurp parse-and-concat ;; parsing
       count-overlapping-areas))       ;; counting
 
+(m/=> isolated-coords [:=> [:cat [:sequential Claim]] [:set Coordinate]])
 (defn- isolated-coords
   "Get the coordinates that are not overlapped by any other claims."
   [claims]
@@ -53,6 +72,7 @@
        keys
        set))
 
+(m/=> isolated-claim [:=> [:cat [:sequential Claim]] :int])
 (defn isolated-claim
   "Find and return the id of first claim that is not overlapped by any other claims."
   [claims]
@@ -63,13 +83,16 @@
          (map :id)
          first)))
 
+(m/=> parse-claims [:=> [:cat :string] [:sequential Claim]])
 (defn parse-claims
   "Parse a list of claims from a string."
   [input]
   (->> input
        str/split-lines
-       (map parse-claim)))
+       (map parse-claim)
+       (filter identity)))
 
+(m/=> day-3-part-2 [:=> [:cat :string] :int])
 (defn day-3-part-2
   "Find the id of the claim that is not overlapped by any other claims."
   [filename]
@@ -81,7 +104,8 @@
   (day-3-part-2 "resources/day_3_input.txt") ; 806
   ;;
 
-  (parse-claim "#1 @ 1,3: 2x2") ; #{{:id 1, :coords [2 3]} {:id 1, :coords [2 4]} {:id 1, :coords [1 4]} {:id 1, :coords [1 3]}}
+  (->> "#1 @ 1,3: 2x2"
+       parse-claim)
   (map :coords #{{:id 1, :coords [1 1]} {:id 1, :coords [1 2]} {:id 1, :coords [1 3]} {:id 1, :coords [1 4]}})  ; ([1 2] [1 1] [1 4] [1 3])
   (into #{} #{[1 2] [2 3]}) ; #{1 2 3}
   (->>  [{:id 1, :coords [1 3]} {:id 1, :coords [1 4]} {:id 1, :coords [2 3]} {:id 1, :coords [2 4]}
