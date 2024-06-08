c19fb5917e500d73b6ed15af1f6d7129fcf3da8f
Hoon Wee <mrgravity817@gmail.com>
2024-06-08 22:42:05 +0900

    test: validate helper functions' schema

--- test/aoc_2018/day_5_test.clj
+++ test/aoc_2018/day_5_test.clj
@@ -1,8 +1,14 @@
 (ns aoc-2018.day-5-test
   (:require [clojure.test :refer [deftest is testing]]
-            [aoc-2018.day-5 :as sut]))
+            [aoc-2018.day-5 :as sut]
+            [malli.core :as m]))
 
 (deftest test-reactable?
+  (testing "schema check"
+    (is (m/validate [:=> [:catn
+                          [:a :symbol]
+                          [:b :symbol]] :boolean]
+                    sut/reactable?)))
   (testing "reactable"
     (is (sut/reactable? \a \A))
     (is (sut/reactable? \B \b)))
@@ -11,6 +17,13 @@
     (is (not (sut/reactable? \B \a)))))
 
 (deftest test-react
+  (testing "schema check"
+    (is (m/validate [:function
+                     [:=> [:cat :string] [:vector :symbol]]
+                     [:=> [:catn
+                           [:acc [:vector :symbol]]
+                           [:chars :string]] [:vector :symbol]]]
+                    sut/react)))
   (testing "react"
     (is (= [] (sut/react "aA")))
     (is (= [] (sut/react "abBA")))
@@ -18,6 +31,9 @@
     (is (= [\d \a \b \C \B \A \c \a \D \A] (sut/react "dabAcCaCBAcCcaDA")))))
 
 (deftest test-remove-alphabet
+  (testing "schema check"
+    (is (m/validate [:=> [:cat :string :symbol] :string]
+                    sut/remove-alphabet)))
   (testing "remove \\a/\\A"
     (is (= "bB"
            (sut/remove-alphabet "abBA" \a)))
