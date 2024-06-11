56ef2c738e5a70b03fcad586d9802f280224df38
Hoon Wee <mrgravity817@gmail.com>
2024-06-12 00:17:19 +0900

    doc: plan how to solve part 2

--- test/aoc_2018/day_7_test.clj
+++ test/aoc_2018/day_7_test.clj
@@ -40,3 +40,16 @@
            (sut/order-steps {"A" #{"C"}, "F" #{"C"}, "B" #{"A"}, "D" #{"A"}, "E" #{"F" "B" "D"}}
                             #{"C" "F" "B" "A" "D" "E"})))))
 
+;; how to solve part 2?
+;; - manage a vector that has 5 slots (for each worker), containing remaining seconds
+;; - each seconds will be decremented for every recursive call, while we accumulate the total seconds at the same time
+;; - if a worker has 0 seconds left, it will be available for the next task (idle)
+;; - idle workers will be assigned to the next task (lexicographically first & doesn't have any preceding steps)
+;; - if all workers are idle, it means we have finished all tasks
+
+;; loop states
+;; - deps and steps
+;; - remaining seconds for each worker (5 slots)
+;; - steps which are being worked on by each worker
+;; - total seconds
+;; - done steps
